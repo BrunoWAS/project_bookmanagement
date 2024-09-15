5f81b403304d88203fbfcb2cd001c6c098bebe42
Bruno.as <b.w.a.sanches@gmail.com>
2024-09-14 21:27:29 -0300

    Adicionando configuração de CORS e outras alterações

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');  // Importe o pacote cors
 const bookRoutes = require('./routes/bookRoutes');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/bookdb', { useNewUrlParser: true, useUnifiedTopology: true });
 
